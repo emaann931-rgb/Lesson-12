de32090829d5f37955dfcf146f2812c7d3a1cfec
Noor13456 <noorgrps@gmail.com>
2025-09-21 13:47:10 +0500

    1

---
+++ activity 1.py
@@ -0,0 +1,32 @@
+#addition on string
+a="20"
+b="50"
+c=a+b
+print(c)
+
+a="50"
+b="20"
+c=int(a)+int(b)
+print(c)
+
+a="Noor"
+b="ulEmaan"
+print(a+b)
+
+a="Noor"
+b="ulEmaan"
+print(a+" "+b)
+
+a="Noor"
+b="Hadiya"
+c="Emaan"
+print(a+" "+b+" "+c)
+
+#multyply string and integer
+a=5
+b="10"
+print(a*b)
+#multyply two integers 
+a=5
+b=10
+print(a*b)
